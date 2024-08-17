e3b8962b05aeb0e8b48fbbac9c7f9bb873b8217b
Hamza Shakeel <hamzashakeel348@gmail.com>
2024-08-17 14:17:28 -0700

    App: Added configs

---
+++ src/config/config.js
@@ -0,0 +1,34 @@
+const config = {
+  sources: [
+    {
+      name: "BBC News",
+      identifier: "bbc-news",
+    },
+    {
+      name: "TechCrunch",
+      identifier: "techcrunch",
+    },
+    {
+      id: "the-next-web",
+      name: "The Next Web",
+    },
+    {
+      identifier: "the-wall-street-journal",
+      name: "The Wall Street Journal",
+    },
+    {
+      id: "google-news",
+      name: "Google News",
+    },
+    {
+      name: "The Guardian",
+      identifier: "the-guardian-uk",
+    },
+    {
+      name: "New York Times",
+      identifier: "the-new-york-times",
+    },
+  ],
+};
+
+export default config;
